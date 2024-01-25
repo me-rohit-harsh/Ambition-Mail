c854426ebf916ff960a5e76dcfd886a533d466d8
Rohit kumar <rohitkumarah369@gmail.com>
2024-01-25 18:03:26 +0530

    responsiveness done

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
-
 // Pricing Section javascript starts
-$(function() {
+$(function () {
 
-	"use strict";
+    "use strict";
 
-	$(".section-pricing .plans-buttons .col-lg-8 > span").on("click", function() {
-		$(this).siblings().removeClass("active").end().addClass("active");
-	});
+    $(".section-pricing .plans-buttons .col-lg-8 > span").on("click", function () {
+        $(this).siblings().removeClass("active").end().addClass("active");
+    });
 
-	$(".section-pricing .header-section .plans-buttons .monthly").on("click", function() {
-		$(".section-pricing .plans .startup .number").text("9");
-		$(".section-pricing .plans .agency .number").text("99");
-		$(".section-pricing .plans .single-plan .plan-name").text("Monthly");
-	});
+    $(".section-pricing .header-section .plans-buttons .monthly").on("click", function () {
+        $(".section-pricing .plans .startup .number").text("9");
+        $(".section-pricing .plans .agency .number").text("99");
+        $(".section-pricing .plans .single-plan .plan-name").text("Monthly");
+    });
 
-	$(".section-pricing .header-section .plans-buttons .yearly").on("click", function() {
-		$(".section-pricing .plans .startup .number").text("99");
-		$(".section-pricing .plans .agency .number").text("999");
-		$(".section-pricing .plans .single-plan .plan-name").text("Yearly");
-	});
+    $(".section-pricing .header-section .plans-buttons .yearly").on("click", function () {
+        $(".section-pricing .plans .startup .number").text("99");
+        $(".section-pricing .plans .agency .number").text("999");
+        $(".section-pricing .plans .single-plan .plan-name").text("Yearly");
+    });
 
 });
 // Pricing section js start 
+
+// testimonial section js starts 
+
+
+$(document).ready(function () {
+    $("#testimonial-slider").owlCarousel({
+        items: 2,
+        itemsDesktop: [1000, 2],
+        itemsDesktopSmall: [979, 2],
+        itemsTablet: [768, 2],
+        itemsMobile: [650, 1],
+        pagination: true,
+        navigation: false,
+        slideSpeed: 1000,
+        autoPlay: true
+    });
+});
+// Testimonial section js ends 
